a4406ac9e81ca5838ef6cc78169997381d2f3561
Oleg Kravchuk <oleg@briolink.com>
2022-01-11 22:15:23 +0200

    BNM-500 Init strub

---
+++ build.gradle.kts
@@ -0,0 +1,82 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version Versions.SPRING_BOOT_VERSION
+    id("io.spring.dependency-management") version Versions.SPRING_DEPENDENCY_MANAGEMENT
+
+    kotlin("jvm") version Versions.KOTLIN_VERSION
+    kotlin("kapt") version Versions.KOTLIN_VERSION
+    kotlin("plugin.spring") version Versions.KOTLIN_VERSION
+
+    `java-library`
+    `maven-publish`
+}
+
+group = "com.briolink"
+version = "0.1.0-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // Spring Boot
+    runtimeOnly("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework:spring-tx")
+    annotationProcessor("org.springframework.boot:spring-boot-autoconfigure-processor")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    kapt("org.springframework.boot:spring-boot-autoconfigure-processor:${Versions.SPRING_BOOT_VERSION}")
+    kapt("org.springframework.boot:spring-boot-configuration-processor:${Versions.SPRING_BOOT_VERSION}")
+
+    // FasterXML
+    api("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
+    api("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+    // Kotlin
+    implementation(kotlin("reflect"))
+    implementation(kotlin("stdlib-jdk8"))
+
+    // kotlin-logging
+    implementation("io.github.microutils:kotlin-logging-jvm:2.0.11")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+
+    withJavadocJar()
+    withSourcesJar()
+}
+
+//publishing {
+//    publications {
+//        create<MavenPublication>("maven") {
+//            from(components["java"])
+//        }
+//    }
+//
+//    repositories {
+//        maven {
+//            name = "Gitlab"
+//            url = uri("https://gitlab.com/api/v4/projects/29889174/packages/maven")
+//
+//            authentication {
+//                create<HttpHeaderAuthentication>("header")
+//            }
+//
+//            credentials(HttpHeaderCredentials::class) {
+//                name = "Deploy-Token"
+//                value = System.getenv("GITLAB_DEPLOY_TOKEN")
+//            }
+//        }
+//        mavenLocal()
+//    }
+//}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "11"
+    }
+}
